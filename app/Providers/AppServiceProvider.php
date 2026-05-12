5117c0826c7dcdaef1fcd6e837be8128244761a4
Defianty Ersa Arfarany <dfntyersa@gmail.com>
2026-05-12 17:11:46 +0700

    Fix: force https and update config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,15 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- Tambahkan ini
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Paksa HTTPS jika bukan di localhost
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
