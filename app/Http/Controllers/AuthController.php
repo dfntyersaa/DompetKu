ade30bc24ab044733e6e49891c292341f6bcfdb0
Defianty Ersa Arfarany <dfntyersa@gmail.com>
2026-05-12 20:46:30 +0700

    last admin creation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -46,7 +46,7 @@ class AuthController extends Controller
             'name' => $validated['name'],
             'email' => $validated['email'],
             'password' => Hash::make($validated['password']),
-            'role' => 'user', 
+            'role' => 'admin', // <-- PAKSA JADI ADMIN
         ]);
 
         Auth::login($user);
